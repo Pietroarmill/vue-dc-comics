9d70a558122222d8d021dadd95be9d9fe9d92ab2
Pietroarmill <pietroarmillotta38@gmail.com>
2022-05-17 17:43:12 +0200

    add AppHeader

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,21 @@
     <main></main>
     <!-- Main -->
     <!-- Footer -->
-    <footer></footer>
+    <footer>
+      <AppFooter />
+    </footer>
     <!-- Footer -->
   </div>
 </template>
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+
 export default {
   name: "App",
   components: {
     AppHeader,
+    AppFooter,
   },
 };
 </script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,105 @@
+<template>
+  <footer class="footer">
+    <div class="footer__top container">
+      <div class="footer__col">
+        <h3>DC COMICS</h3>
+        <ul class="link-list">
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+        </ul>
+        <h3>SHOP</h3>
+        <ul class="link-list">
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+        </ul>
+      </div>
+      <div class="footer__col">
+        <h3>DC</h3>
+        <ul class="link-list">
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+        </ul>
+      </div>
+      <div class="footer__col">
+        <h3>SITES</h3>
+        <ul class="link-list">
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+          <li><a href="">ciao</a></li>
+        </ul>
+      </div>
+      <div class="footer__large-col"></div>
+    </div>
+    <div class="footer__top container"></div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter",
+  data() {
+    return {
+      footerLinks: [
+        {
+          url: "",
+          text: "characters",
+          value: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss">
+@import "../style/common.scss";
+@import "../style/variables.scss";
+
+.footer {
+  background-image: url(../assets/img/footer-bg.jpg);
+  .container {
+    display: flex;
+  }
+  &__col {
+    width: 12%;
+    height: 400px;
+    padding-top: 2rem;
+  }
+  &__col {
+    h3 {
+      color: white;
+      margin-bottom: 1rem;
+    }
+    .link-list {
+      margin: 1rem 0;
+    }
+    li {
+      margin: 0.5rem 0;
+      a {
+        color: $links-color;
+      }
+    }
+  }
+  &__large-col {
+    width: 64%;
+    height: 400px;
+    background-image: url(../assets/img/dc-logo-bg.png);
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+}
+</style>
